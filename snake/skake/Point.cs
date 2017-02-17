ff062514651bcdd30fc0af8a01d9fad90f6c7ad2
wnikiti4 <wnikiti4@gmail.com>
2017-02-17 23:55:02 +0300

    added class Point (x y char_point) added comment code

---
+++ snake/skake/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Point
+    {
+       public int x;
+       public int y;
+       public char sym;
+
+        public void Draw( int x = 0, int y = 0, char sym = '@' )        //метод класса Point (+ значения по умолчанию)
+        {
+            Console.SetCursorPosition(x, y);                            // установка курсора на значение координат
+            Console.Write(sym);                                         //вывод символа Sym 
+        }
+    }
+}
--- snake/skake/Program.cs
+++ snake/skake/Program.cs
@@ -10,8 +10,13 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("тест");
-            Console.ReadLine();
+            Point p1 = new Point();  //  выделение памяти под структуру Point и создание обьекта P1 
+            p1.x = 23;               //  инициализация значений в оъекте P1 значения x 
+            p1.y = 21;               //  инициализация значений в оъекте P1 значения y 
+                                     //  инициализация значения sym, Происходит по умолчанию в классе Point 
+            p1.Draw(p1.x,p1.y);     //  вызов метода Draw класса Point
+            Console.ReadLine();     //  заглушка 
         }
+      
     }
 }
